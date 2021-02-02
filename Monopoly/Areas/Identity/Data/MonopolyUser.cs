7616d9806c707c2d9af638cee72ae3ffc27febdd
Denisa Francu <denisa_francu@trimble.com>
2021-02-02 10:11:41 +0200

    Initial commit

---
+++ Monopoly/Areas/Identity/Data/MonopolyUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Monopoly.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the MonopolyUser class
+    public class MonopolyUser : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string FirstName { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string LastName { get; set; }
+    }
+}
---
+++ Monopoly/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Monopoly.Areas.Identity.Data;
+using Monopoly.Data;
+
+[assembly: HostingStartup(typeof(Monopoly.Areas.Identity.IdentityHostingStartup))]
+namespace Monopoly.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<MonopolyDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("MonopolyDbContextConnection")));
+
+                services.AddDefaultIdentity<MonopolyUser>(options => {
+                    options.SignIn.RequireConfirmedAccount = false; //no email confirmation
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    }) 
+                    .AddEntityFrameworkStores<MonopolyDbContext>();
+            });
+        }
+    }
+}
